48033c9c4fdf8edf511468a7587e5ed878d43f0c
user <user@users-Mac-mini.local>
2022-12-06 18:25:47 +0300

    init: initial commit;

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by Mikhail Prokofiev
+ * Date: 09/12/2022
+ */
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+    //DefaultController
+}
